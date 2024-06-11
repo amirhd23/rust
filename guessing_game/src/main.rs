ad21bc5979ab66642fece23d0fbbb76a204fc2a2
Amir Drs <amir.deris67@gmail.com>
2024-06-10 21:04:19 -0700

    fixed formatting

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,26 +1,23 @@
-use std::io;
 use rand::Rng;
 use std::cmp;
+use std::io;
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("secret number is {secret_number}");
-
     println!("Guess a number between 1 to 100");
 
-    
-    
     loop {
         println!("Please input your guess");
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("failed to read the guess");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read the guess");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        
+
         match guess.cmp(&secret_number) {
             cmp::Ordering::Greater => println!("Too big!"),
             cmp::Ordering::Less => println!("Too small!"),
